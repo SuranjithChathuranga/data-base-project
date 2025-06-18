e263bbeb378041269baf27a4ed67400b7980cee7
Suranjith Chathuranga <149863436+SuranjithChathuranga@users.noreply.github.com>
2025-06-18 12:47:07 +0530

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,194 @@
+﻿namespace ProjectDAD
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.Smartmovers = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnsave = new System.Windows.Forms.Button();
+            this.btnedit = new System.Windows.Forms.Button();
+            this.btndelete = new System.Windows.Forms.Button();
+            this.txtid = new System.Windows.Forms.TextBox();
+            this.txtname = new System.Windows.Forms.TextBox();
+            this.txttype = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // Smartmovers
+            // 
+            this.Smartmovers.AutoSize = true;
+            this.Smartmovers.BackColor = System.Drawing.Color.Transparent;
+            this.Smartmovers.Font = new System.Drawing.Font("Microsoft YaHei UI", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Smartmovers.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.Smartmovers.Location = new System.Drawing.Point(309, 39);
+            this.Smartmovers.Name = "Smartmovers";
+            this.Smartmovers.Size = new System.Drawing.Size(490, 86);
+            this.Smartmovers.TabIndex = 0;
+            this.Smartmovers.Text = "Smart Movers";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label2.Location = new System.Drawing.Point(277, 150);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(103, 18);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Customer ID";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label3.Location = new System.Drawing.Point(277, 199);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(128, 18);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Customer name";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label4.Location = new System.Drawing.Point(277, 241);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(118, 18);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Customer type";
+            // 
+            // btnsave
+            // 
+            this.btnsave.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.btnsave.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnsave.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.btnsave.Location = new System.Drawing.Point(118, 351);
+            this.btnsave.Name = "btnsave";
+            this.btnsave.Size = new System.Drawing.Size(181, 52);
+            this.btnsave.TabIndex = 1;
+            this.btnsave.Text = "Save";
+            this.btnsave.UseVisualStyleBackColor = false;
+            this.btnsave.Click += new System.EventHandler(this.btnsave_Click);
+            // 
+            // btnedit
+            // 
+            this.btnedit.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.btnedit.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnedit.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.btnedit.Location = new System.Drawing.Point(430, 351);
+            this.btnedit.Name = "btnedit";
+            this.btnedit.Size = new System.Drawing.Size(181, 52);
+            this.btnedit.TabIndex = 1;
+            this.btnedit.Text = "Update";
+            this.btnedit.UseVisualStyleBackColor = false;
+            this.btnedit.Click += new System.EventHandler(this.btnedit_Click);
+            // 
+            // btndelete
+            // 
+            this.btndelete.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.btndelete.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btndelete.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.btndelete.Location = new System.Drawing.Point(722, 351);
+            this.btndelete.Name = "btndelete";
+            this.btndelete.Size = new System.Drawing.Size(181, 52);
+            this.btndelete.TabIndex = 1;
+            this.btndelete.Text = "Delete";
+            this.btndelete.UseVisualStyleBackColor = false;
+            this.btndelete.Click += new System.EventHandler(this.btndelete_Click);
+            // 
+            // txtid
+            // 
+            this.txtid.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.txtid.Location = new System.Drawing.Point(487, 150);
+            this.txtid.Name = "txtid";
+            this.txtid.Size = new System.Drawing.Size(278, 20);
+            this.txtid.TabIndex = 2;
+            // 
+            // txtname
+            // 
+            this.txtname.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.txtname.Location = new System.Drawing.Point(487, 199);
+            this.txtname.Name = "txtname";
+            this.txtname.Size = new System.Drawing.Size(278, 20);
+            this.txtname.TabIndex = 2;
+            // 
+            // txttype
+            // 
+            this.txttype.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.txttype.Location = new System.Drawing.Point(487, 241);
+            this.txttype.Name = "txttype";
+            this.txttype.Size = new System.Drawing.Size(278, 20);
+            this.txttype.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(1030, 480);
+            this.Controls.Add(this.txttype);
+            this.Controls.Add(this.txtname);
+            this.Controls.Add(this.txtid);
+            this.Controls.Add(this.btndelete);
+            this.Controls.Add(this.btnedit);
+            this.Controls.Add(this.btnsave);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.Smartmovers);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label Smartmovers;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnsave;
+        private System.Windows.Forms.Button btnedit;
+        private System.Windows.Forms.Button btndelete;
+        private System.Windows.Forms.TextBox txtid;
+        private System.Windows.Forms.TextBox txtname;
+        private System.Windows.Forms.TextBox txttype;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace ProjectDAD
+{
+    public partial class Form1 : Form
+    {
+
+        SqlConnection connection = new SqlConnection(@"Data Source=(LocalDB)\v11.0;AttachDbFilename=C:\Users\PUBLIC PC\Desktop\ASG\sharanya\DAD\ProjectDAD\ProjectDAD\smartmovers.mdf;Integrated Security=True");
+        SqlCommand command;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void openconnection()
+        {
+            if(connection.State == ConnectionState.Closed)
+            {
+                connection.Open();
+            }
+
+        }
+
+        private void closeconnection()
+        {
+            if(connection.State == ConnectionState.Open)
+            {
+                connection.Close();
+            }
+        }
+
+        public void executequery(string query)
+        {
+            try
+            {
+                openconnection();
+                command = new SqlCommand(query, connection);
+
+                if(command.ExecuteNonQuery() == 1)
+                {
+                    MessageBox.Show("Successfully saved!!!");
+                }
+                else
+                {
+                    MessageBox.Show("Saved unsuccessfull!!!");
+                }
+
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                closeconnection();
+            }
+        }
+
+        private void btnedit_Click(object sender, EventArgs e)
+        {
+            string updatequery = "update customer set cusid=" + txtid.Text + ",cusname='" + txtname.Text + "',type='" + txttype.Text + "' where cusid=" + txtid.Text + "";
+            executequery(updatequery);
+        }
+
+        private void btndelete_Click(object sender, EventArgs e)
+        {
+            string deletequery = "delete from customer where cusid="+txtid.Text+"";
+            executequery(deletequery);
+        }
+
+        private void btnsave_Click(object sender, EventArgs e)
+        {
+           
+            string insertQuery = "insert into customer(cusid,cusname,type) values ("+txtid.Text+",'"+txtname.Text+"','"+txttype.Text+"')";
+            executequery(insertQuery);
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
